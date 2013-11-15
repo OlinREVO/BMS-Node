2e5e3ba3a2519400d0b13ca1f3410b63fb3a690e
MattWis <matthew.wismer@students.olin.edu>
2013-11-15 10:45:41 -0500

    Used arrays to choose input and output pins

--- src/main.c
+++ src/main.c
@@ -9,6 +9,12 @@
 //int sendCANmsg(int destID, int msgID, char msg[], int msgLen);
 
 #define OVER_VOLTAGE_LED _BV(PB7) //Gets turned on, stays on
+
+// inputs determines which ADC to use to read cell voltage
+uint8_t inputs[] = { 0x01, 0x02, 0x03, 0x04 };
+// outputs determines which pin in PORTB to use to shunt the cell
+uint8_t outputs[] = { _BV(PB6), _BV(PB5), _BV(PB4), _BV(PB3) };
+
 int main (void) {
     //Enable ADC, set prescalar to 128 (slow down ADC clock)
     ADCSRA |= _BV(ADEN) | _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0);
@@ -29,7 +35,7 @@ int main (void) {
         for (ch = 0; ch < 4; ch++) {
 
             //the low 4 bits of ADMUX select the ADC channel
-            ADMUX |= ch;
+            ADMUX |= inputs[ch];
             //Wait for ADC reading
             ADCSRA |=  _BV(ADSC);
             while(bit_is_set(ADCSRA, ADSC));
@@ -37,18 +43,18 @@ int main (void) {
             //ADC is a macro to combine ADCL and ADCH
             uint16_t voltage = ADC;
 
-            //ch selects bits of PORTB (0 - 3)
-            if (voltage >= MAXV) {
-                PORTB |= (1 << ch);
+            if (voltage >= MAXV){
+                PORTB |= outputs[ch];
                 PORTB |= OVER_VOLTAGE_LED;
                 sendCANmsg(NODE_watchdog,MSG_shunting,'0',1);
             }
-            else if (voltage <= MINV){
+            else if (voltage <= MINV) {
+                PORTB &= ~outputs[ch];
                 //Sending low voltage signal over CAN
                 sendCANmsg(NODE_watchdog,MSG_voltagelow,'0',1);
             }
             else {
-                PORTB &= ~(1 << ch);
+                PORTB &= ~outputs[ch];
             }
             _delay_ms(1);
         }
