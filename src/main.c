854119e01d3347db1f8bf52ef6036c330e744050
Matt Wismer <mattwis86@gmail.com>
2015-01-31 11:50:25 -0500

    Clean initialization code

--- src/main.c
+++ src/main.c
@@ -13,46 +13,47 @@ uint8_t inputs[] = { 0x06, 0x05, 0x03, 0x04, 0x01, 0x02};
 // outputs determines which pin in PORTB to use to shunt the cell
 uint8_t outputs[] = { _BV(PD1), _BV(PC0), _BV(PD0), _BV(PB3), _BV(PB4), _BV(PC7)};
 
+void init_io_pins() {
+    // Set all of PORTB (except PB7 - reading from cell) to output low
+    DDRB = 0x7F;
+    PORTB &= 0x80;
 
-int main (void) {
+    //setting inputs
+    DDRD = 0x00;
+    DDRD &= ~(_BV(PD5)); //input 1
+    DDRD &= ~(_BV(PD6));
+    DDRB &= ~(_BV(PB2));
+    DDRB &= ~(_BV(PB7));
+    DDRB &= ~(_BV(PB6));
+    DDRB &= ~(_BV(PB5));
+
+    //setting outputs
+    DDRD |= _BV(PD1);
+    DDRC |= _BV(PC0);
+    DDRD |= _BV(PD0);
+    DDRB |= _BV(PB3);
+    DDRB |= _BV(PB4);
+    DDRC |= _BV(PC7);
+}
+
+void initADC() {
     //Enable ADC, set prescalar to 128 (slow down ADC clock)
     ADCSRA |= _BV(ADEN) | _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0);
     //Enable internal reference voltage
     ADCSRB &= _BV(AREFEN);
     //Set internal reference voltage as AVcc
     ADMUX |= _BV(REFS0);
+}
 
-    // Set all of PORTB (except PB7 - reading from cell) to output low
-    DDRB = 0x7F;
-
-    PORTB &= 0x80;
-
-    DDRD = 0x00;
-
-    //setting inputs 
-    DDRD &=~_BV(PD5); //input 1
-    DDRD &=~_BV(PD6);
-    DDRB &=~_BV(PB2);
-    DDRB &=~_BV(PB7);
-    DDRB &=~_BV(PB6);
-    DDRB &=~_BV(PB5); 
-
-    //setting outputs 
-    DDRD |=_BV(PD1); 
-    DDRC |=_BV(PC0); 
-    DDRD |=_BV(PD0);
-    DDRB |=_BV(PB3);
-    DDRB |=_BV(PB4);
-    DDRC |=_BV(PC7);
-
-
+int main (void) {
+    init_io_pins();
+    initADC();
     initCAN(NODE_bms);
+
     uint8_t ch; //Selects ADC Channel and PORTB write output
     uint8_t msg[1];
     //Loop Begins
     for (;;) {
-//uint8_t ports[] = {PORTD, PORTC, PORTD, PORTB, PORTB, PORTC};
-
         //Check each of the 6 cells
         for (ch = 0; ch < 6; ch++) {
 
@@ -93,6 +94,5 @@ int main (void) {
     return 1;
 }
 
-// TODO: change this method for each of the demo nodes
 void handleCANmsg(uint8_t destID, uint8_t msgID, uint8_t* msg, uint8_t msgLen) {}
 
